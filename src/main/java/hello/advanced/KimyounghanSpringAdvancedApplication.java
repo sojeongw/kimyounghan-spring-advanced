2784f822232291d7b574b0e986ea2d47579cb3b7
sojeongw <sojeongw.official@gmail.com>
2025-03-13 10:03:37 +0900

    예제 프로젝트 만들기

--- src/main/java/hello/advanced/KimyounghanSpringAdvancedApplication.java
+++ src/main/java/hello/advanced/KimyounghanSpringAdvancedApplication.java
@@ -1,9 +1,13 @@
 package hello.advanced;
 
+import hello.advanced.config.AppV1Config;
+import hello.advanced.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@Import({AppV1Config.class, AppV2Config.class})
+@SpringBootApplication(scanBasePackages = "hello.advanced.proxy")
 public class KimyounghanSpringAdvancedApplication {
 
     public static void main(String[] args) {
